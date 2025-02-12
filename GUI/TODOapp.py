d86559233b85d77e717ae7839980e2bdc14ce789
no-logo <andrzej.wawryk@gmial.com>
2025-02-12 08:58:25 +0100

    praca w domu 2025-02-12

--- GUI/TODOapp.py
+++ GUI/TODOapp.py
@@ -4,7 +4,6 @@ from DataBase.sqlalchemy_todo_db import TODO_db, connect_url
 
 todo_db = TODO_db(connect_url)
 
-
 class ButtonFrameList(ctk.CTkFrame):
     def __init__(self, master, radio_frame_list):
         super().__init__(master)
